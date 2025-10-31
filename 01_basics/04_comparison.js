ed64f116ad5c3e4e08cdb2d6345d5bb6c36971d7
Aniket Polkar <142425052+Aniketpolkar@users.noreply.github.com>
2025-10-31 11:01:59 +0000

    datatypes comparisons  and type conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(null > 0)//false
+console.log(null == 0)//false
+console.log(null >= 0)//true
+
+// for == and === the functions working is different that <>= opeators
+
+console.log(undefined == 0)//false for any above check
+
+console.log("2"==2)
